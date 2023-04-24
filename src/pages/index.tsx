0f2db1f8a3a21fce7955a36325d65902dfb7b473
guidolarosa <guido.glarosa@gmail.com>
2023-04-24 18:49:44 -0300

    Updated animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,58 @@
-import Image from 'next/image';
-import { Montserrat } from 'next/font/google';
-import Link from 'next/link';
-import Box from './../components/Box/Box';
-import Button from './../components/Button/Button';
-import { navigation } from '@/localization/en/ui-constants';
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import Link from "next/link";
+import Box from "./../components/Box/Box";
+import Button from "./../components/Button/Button";
+import { navigation } from "@/localization/en/ui-constants";
 
-const montserrat = Montserrat({ subsets: ['latin'] });
+import gsap from "gsap";
+import { useEffect } from "react";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
+  let stagger = 2;
+  let animation = gsap.timeline({
+    repeat: -1,
+    repeatDelay: -5,
+  });
+
+  useEffect(() => {
+    animation
+      .from(".rotating-text", {
+        y: 240,
+        opacity: 0,
+        stagger: stagger,
+        filter: "blur(10px)",
+      })
+      .to(
+        ".rotating-text",
+        {
+          y: -240,
+          opacity: 0,
+          stagger: stagger,
+          filter: "blur(10px)",
+        },
+        stagger
+      );
+  });
+
   return (
-    <div
-      className={`${montserrat.className} h-[2000px]`}
-    >
+    <div className={`${montserrat.className} h-[2000px]`}>
       <Box id="header" className="h-12 my-8 flex items-center fixed">
         <div className="relative h-8 w-8">
-          <Image 
-            fill
-            src="/logo.svg"
-            alt="Volcan"
-          />
+          <Image fill src="/logo.svg" alt="Volcan" />
         </div>
         <nav className="rounded-full border border-stone-600 ml-8 px-8 h-16 items-center justify-center bg-stone-900 hidden md:flex backdrop-opacity-100">
           <ul className="flex gap-8">
             {navigation.map((item) => (
-              <li key={item.href} className={'tracking-widest font-light text-stone-300 hover:text-stone-100 transition-all'}>
-                <Link href={item.label}>
-                  {item.label}
-                </Link>
+              <li
+                key={item.href}
+                className={
+                  "tracking-widest font-light text-stone-300 hover:text-stone-100 transition-all"
+                }
+              >
+                <Link href={item.label}>{item.label}</Link>
               </li>
             ))}
           </ul>
@@ -34,22 +60,41 @@ export default function Home() {
       </Box>
       <main>
         <Box className="flex h-[calc(100vh)] items-center justify-center">
-          <strong className="text-6xl lg:text-9xl text-red-600">Imagine</strong>
+          <strong className="rotating-text absolute text-6xl lg:text-9xl text-rose-500 blur-0">
+            Imagine
+          </strong>
+          <strong className="rotating-text absolute text-6xl lg:text-9xl text-orange-500 blur-0">
+            Create
+          </strong>
+          <strong className="rotating-text absolute text-6xl lg:text-9xl text-violet-500 blur-0">
+            Develop
+          </strong>
         </Box>
         <Box className="">
-          <div id="breakdown" className="flex flex-col max-w-6xl mx-auto w-full text-center py-16 rounded-2xl bg-cover" style={{backgroundImage: `url(/homepage/breakdown.png)`}}>
-            <strong className="inline-block text-5xl lg:text-8xl">We Are</strong>
-            <strong className="inline-block text-5xl lg:text-8xl">VOLCAN</strong>
+          <div
+            id="breakdown"
+            className="flex flex-col max-w-6xl mx-auto w-full px-2 text-center py-16 rounded-2xl bg-cover"
+            style={{ backgroundImage: `url(/homepage/breakdown.png)` }}
+          >
+            <strong className="inline-block text-5xl lg:text-8xl">
+              We Are
+            </strong>
+            <strong className="inline-block text-5xl lg:text-8xl">
+              VOLCAN
+            </strong>
             <p className="text-2xl lg:text-4xl mt-8 leading-relaxed max-w-4xl mx-auto tracking-wide">
-              We create and develop business ideas <br/>and brands with <strong>attitude and <br/>meaningfull impact</strong>.
+              We create and develop business ideas <br />
+              and brands with{" "}
+              <strong>
+                attitude and <br />
+                meaningfull impact
+              </strong>
+              .
             </p>
-            <Button
-              label={'About Us'}
-              className={'mt-8 mx-auto'}
-            />
+            <Button label={"About Us"} className={"mt-8 mx-auto"} />
           </div>
         </Box>
       </main>
     </div>
-  )
+  );
 }
